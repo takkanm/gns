7f7b7d2d4fbf76240502e0875fbccb9e5e50e42a
Mitsutaka Mimura <takkanm@gmail.com>
2012-10-15 00:12:35 +0900

    create github rss class

---
+++ lib/gns/github_rss.rb
@@ -0,0 +1,37 @@
+require 'rss'
+require 'open-uri'
+
+class GithubRss
+  def initialize(url, start_time = Time.now)
+    @url = url
+    @last_updated = start_time
+  end
+
+  def recent_items
+    get_rss_items.find_all {|item|
+      item.updated > @last_updated
+    }.tap {|items|
+      @last_updated = items.first.updated if items
+    }
+  end
+
+  def get_rss_items
+    RSS::Parser.parse(open(@url)).items.map {|item|
+      GithubItem.new(item)
+    }
+  end
+end
+
+class GithubItem
+  attr_reader :updated, :title, :url, :body, :repo
+
+  def initialize(rss_item)
+    @updated = rss_item.updated.content
+    @title = rss_item.title.content
+    @url = rss_item.link.href
+    rss_item.content.content =~ /<p>(.*)<\/p>/
+    @body = $1
+    rss_item.content.content =~ /<a.*>(.*)<\/a>/
+    @repo = $1
+  end
+end
