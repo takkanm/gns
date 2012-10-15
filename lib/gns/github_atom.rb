9e715bffd7ecc55e27bc5ffb41c596761bf9a432
Mitsutaka Mimura <takkanm@gmail.com>
2012-10-15 10:11:17 +0900

    rename rss to atom

--- lib/gns/github_atom.rb
+++ lib/gns/github_atom.rb
@@ -2,21 +2,21 @@ require 'rss'
 require 'open-uri'
 
 module Gns
-  class GithubRss
+  class GithubAtom
     def initialize(url, start_time = Time.now)
       @url = url
       @last_updated = start_time
     end
 
     def recent_items
-      get_rss_items.find_all {|item|
+      get_atom_items.find_all {|item|
         item.updated > @last_updated
       }.tap {|items|
         @last_updated = items.first.updated if items.first
       }
     end
 
-    def get_rss_items
+    def get_atom_items
       RSS::Parser.parse(open(@url)).items.map {|item|
         GithubItem.new(item)
       }
@@ -26,13 +26,13 @@ module Gns
   class GithubItem
     attr_reader :updated, :title, :url, :body, :repo
 
-    def initialize(rss_item)
-      @updated = rss_item.updated.content
-      @title = rss_item.title.content
-      @url = rss_item.link.href
-      rss_item.content.content =~ /<p>(.*)<\/p>/
+    def initialize(atom_item)
+      @updated = atom_item.updated.content
+      @title = atom_item.title.content
+      @url = atom_item.link.href
+      atom_item.content.content =~ /<p>(.*)<\/p>/
       @body = $1
-      rss_item.content.content =~ /<a.*>(.*)<\/a>/
+      atom_item.content.content =~ /<a.*>(.*)<\/a>/
       @repo = $1
     end
   end
